5a1d22753f36d8cbf5556ff97f84e4ee49393856
Nikolay <@>
2021-11-14 16:49:08 +0200

    Add TK_1.py and create get_from_console() function

---
+++ TK_1.py
@@ -0,0 +1,7 @@
+def get_from_console(list):
+    numb_arr: list[int] = []
+    for i in range(0, list):
+        numb = int(input('Enter Ints'))
+        numb_arr.append(numb)
+
+        return numb_arr
